927598a807c9c497229d1ad6a117539209b8c7f4
Ashish Dayama <68162230+AshishDayama@users.noreply.github.com>
2022-06-15 15:10:44 +0530

    Add files via upload

---
+++ odd_even.rb
@@ -0,0 +1,16 @@
+#odd or even
+class Test
+	def even_odd(number)
+		
+		if number.even?=="true"
+			puts"Its even"
+		else
+			puts"Its odd"
+		end
+		
+	end
+end
+puts" give a number to check"
+number=gets.to_i
+obj=Test.new
+obj.even_odd(number)
