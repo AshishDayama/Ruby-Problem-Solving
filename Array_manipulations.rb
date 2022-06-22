06621630eac8cd984e394eb95cc61e59ffbf0a71
Ashish <adityakareshia16@gmail.com>
2022-06-22 13:22:10 +0530

    Manipulating arrays

---
+++ Array_manipulations.rb
@@ -0,0 +1,21 @@
+#creating new array
+array=[]
+print array
+
+#creanting array using "Array.new"
+#array1=Array.new
+#print array1
+
+#adding elements "10,10" to array1
+array1=Array.new(10,2)
+puts array1
+print array1
+p array1
+
+#using insert
+array.insert(2,15)
+print array
+
+#using push
+array1.push(13)
+p array1
