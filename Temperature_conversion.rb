7a5d367a31d8c36b1bb7de331b8886972b438df0
Ashish <adityakareshia16@gmail.com>
2022-07-14 18:23:14 +0530

    Have to implement double splat in it, not completed it!

---
+++ Temperature_conversion.rb
@@ -0,0 +1,20 @@
+class Test
+    def convert_temp(temperature, output_scale)
+        
+       
+        if output_scale=='kelvin'
+            return temperature+32.0
+        elsif output_scale=='fahrenheit'
+            return temperature+274.15
+        else
+            return temperature
+        end
+    end
+end 
+print "Enter temperature in celcius  "
+temperature=gets.to_f
+obj=Test.new
+val=obj.convert_temp(temperature,'kelvin')
+puts val
+
+
